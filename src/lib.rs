79ba080d4f2da0346c868884bb57bcf3d51df588
dskyberg <davidskyberg@gmail.com>
2022-02-27 10:08:15 -0500

    Removed unused KeyPair mod.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! KT performs very simply key display and conversion functions with 100% Rust code.
+//! 
+//! KT leverages the [formats](docs.rs/formats) crates from RustCrypto to manage public/private keys in
+//! various formats.  To see the full list, run `kt convert --help`
+//! 
 pub mod alg_id;
 pub mod app_state;
 pub mod cli;
@@ -6,6 +11,5 @@ pub mod discover;
 pub mod document;
 pub mod errors;
 pub mod key_info;
-pub mod keypair;
 pub mod oids;
 
